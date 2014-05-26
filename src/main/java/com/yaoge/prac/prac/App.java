e7ae107aa08652659710b354e3546ad7dfbcef5f
yaogenum <yaogemail@gmail.com>
2014-05-26 19:21:41 +0800

    cluster第三次提交代码

--- src/main/java/com/yaoge/prac/prac/App.java
+++ src/main/java/com/yaoge/prac/prac/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello 12122World万1212千瓦!" );
+        System.out.println( "Hello 12122World万1212千瓦!cluster" );
     }
 }
