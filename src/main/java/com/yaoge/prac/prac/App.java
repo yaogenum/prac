57af111f6ae60a1fa40d31a2116afaf18383d8ff
yaogenum <yaogemail@gmail.com>
2014-05-26 18:35:27 +0800

    cluster分支第一次提交

--- src/main/java/com/yaoge/prac/prac/App.java
+++ src/main/java/com/yaoge/prac/prac/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello 122World万1212千瓦!" );
+        System.out.println( "Hello 12122World万1212千瓦!" );
     }
 }
