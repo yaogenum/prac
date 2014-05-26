214a99fec35446e27ff9ffd4b375d56d49382f53
yaoge <yaogemail@gmail.com>
2014-05-26 23:44:33 +0800

    server4
    server4第四次测试编写

--- src/main/java/com/yaoge/prac/prac/App.java
+++ src/main/java/com/yaoge/prac/prac/App.java
@@ -16,5 +16,6 @@ public class App
         System.out.println( "Hello 122Wo1222rld万1212千瓦!" );
         System.out.println("server second commit!!!");
         System.out.println("server third测试使用!!!");
+        System.out.println("server four测试使用!!!");
     }
 }
