6d539e3ba5ef4c3fb7a4712b796429d63accceea
yaogenum <yaogemail@gmail.com>
2014-05-31 20:10:48 +0800

    第13次修改

--- src/main/java/com/yaoge/prac/prac/ABC.java
+++ src/main/java/com/yaoge/prac/prac/ABC.java
@@ -1,10 +1,13 @@
 package com.yaoge.prac.prac;
 
-public class ABC {
+import java.io.Serializable;
+
+public class ABC implements Serializable{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("AD12B");
 	}
+
 
 }
