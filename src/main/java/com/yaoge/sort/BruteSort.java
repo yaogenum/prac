2241e65a12aab9e2d751c71e7fe861bdeb67dba4
yaogenum <yaogemail@gmail.com>
2014-09-13 22:18:55 +0800

    2014/9/13 快速排序算法

--- src/main/java/com/yaoge/sort/BruteSort.java
+++ src/main/java/com/yaoge/sort/BruteSort.java
@@ -1,25 +1,53 @@
 package com.yaoge.sort;
 
-public class BruteSort {
+import com.yaoge.inter.Sort;
+
+public class BruteSort implements Sort{
 	
-	/**
-	 * 提出非法子元素
-	 * @param sourceArray
-	 * @return
-	 */
-	public int[] inputSort(int[] sourceArray) {
+	public  int[] inputSort(int[] sourceArray) {
 		
+		if(sourceArray.length<=0){
+			return null;
+		}
+		else{
+			return sourceArray.clone();
+		}
+	}
+	public  int[] outputSort(int targetArray[]) {
 		
+		if(targetArray.length<=0) {
+			return null;
+		}
+		else{
+			return targetArray.clone();
+		}	
 		
-		return null;
+	}
+	public int[] startSort(int[] centerArray) {
+		
+		int length=centerArray.length;
+		int tmp;
+		for(int i=0 ; i<length ; i++) {
+			
+			for(int j=i+1 ; j<length ; j++) {
+				
+				if(centerArray[i] < centerArray[j]) {
+					tmp = centerArray[i] ;
+					centerArray[i] = centerArray[j] ;
+					centerArray[j] = tmp ;
+				}
+				
+			}
+		}
+		
+		return centerArray;
 	}
 	
-	public int[] outputSort() {
-		return null;
+	public <T> void swapItem(T[] swapArray,int t1,int t2) {
+		T tmp=swapArray[t2];
+		swapArray[t2]=swapArray[t1];
+		swapArray[t1]=tmp;
 	}
 	
-	public int[] startSort() {
-		return null;
-	}
 	
 }
--- src/main/java/com/yaoge/sort/QuickSort.java
+++ src/main/java/com/yaoge/sort/QuickSort.java
@@ -1,5 +1,95 @@
 package com.yaoge.sort;
 
-public class QuickSort {
+import com.yaoge.inter.Sort;
 
+public class QuickSort implements Sort {
+
+	public int[] inputSort(int[] sourceArray) {
+		// TODO Auto-generated method stub
+		if(sourceArray.length<=0){
+			return null;
+		}
+		else{
+			return sourceArray.clone();
+		}
+	}
+
+	public int[] outputSort(int[] targetArray) {
+		// TODO Auto-generated method stub
+		if(targetArray.length<=0) {
+			return null;
+		}
+		else{
+			return targetArray.clone();
+		}	
+		
+	}
+
+	public int[] startSort(int[] centerArray, int start, int end) {
+		// TODO Auto-generated method stub
+		if (start < end) {
+			
+			int flag = sortByMiddle(centerArray, start, end);
+			startSort(centerArray, start, flag - 1);
+			startSort(centerArray, flag + 1, end);
+			
+			
+		}
+
+		return centerArray;
+	}
+
+	public int sortByMiddle(int[] minArray, int start, int end) {
+		int flag;
+		flag = minArray[start];
+
+		while (start < end) {
+
+			while (start < end && minArray[end] >= flag) {
+				end--;
+			}
+			minArray[start] = minArray[end];
+			
+			while (start < end && minArray[start] < flag) {
+				start++;
+			}
+			minArray[end] = minArray[start] ;
+			
+		}
+		
+		minArray[start] = flag ;
+
+		return start;
+
+	}
+
+	public <T> void swapItem(T[] swapArray, int t1, int t2) {
+		// TODO Auto-generated method stub
+
+	}
+	public int[] startSort(int[] centerArray) {
+		
+		startSort(centerArray,0,centerArray.length-1);
+		return centerArray;
+	}
+
+	public static void main(String[] args ) {
+		
+		QuickSort sort = new QuickSort();
+		int array[]={1,2,1,10,45,100,56,12,56,34,78,12,89,1,110,45,67,12};
+	
+		System.out.println("长度："+array.length);
+		array = sort.startSort(array);
+		
+		
+		
+		for(int s : array) {
+			System.out.println(s);
+		}
+		System.out.println("长度："+array.length);
+		System.out.println("ok");
+	
+	}
+
+	
 }
